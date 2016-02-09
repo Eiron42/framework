6680a07c72947d37fd40af45d7e297c1341c5c72
Eiron <dufourclement1811@msn.com>
2016-02-09 22:12:04 +0100

    Added a basic routing

--- app.php
+++ app.php
@@ -1,3 +1,29 @@
 <?php
 
-echo $path = $_SERVER['PATH_INFO'];
+use Symfony\Component\Yaml\Yaml;
+
+$path = $_SERVER['PATH_INFO'];
+$currentBlock = null;
+$currentRoute = null;
+
+// Getting global routing and list of blocks' routing to include
+$globalRouting = Yaml::parse(file_get_contents(__DIR__ . '/config/routing.yml'));
+//$routes['global'] = $globalRouting['routing'];
+
+foreach ($globalRouting['blocksRouting'] as $block => $prefix) {
+    $blockRoutes = Yaml::parse(file_get_contents(__DIR__ . '/src/'. $block .'/routing.yml'));
+    foreach ($blockRoutes as $route => $data) {
+        $data['path'] = $prefix . $data['path'];
+        $blockRoutes[$route] = $data;
+        if ($path === $data['path']) {
+            $currentBlock = $block;
+            $currentRoute = $data;
+            break;
+        }
+    }
+    if ($currentRoute === null) {
+        $currentRoute = 404;
+    }
+}
+
+var_dump($currentRoute);
